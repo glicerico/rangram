b4c35b53b458bd86c350fb407c669e7bd7f42fa3
glicerico <suarezandres@gmail.com>
2019-10-08 14:56:13 +0800

    process_SP is working

--- utils/process_SP.sh
+++ utils/process_SP.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Processes a corpus from a given grammar with stream-parser and evaluates the results:
-# SP: stream-parser
 
 # Usage process_SP.sh <grammar_name> <maxWinObserve> <maxWinParse>
 
@@ -10,26 +9,19 @@ gram_name=$1
 maxWinObserve=$2
 maxWinParse=$3
 
-rangram_workdir="/home/andres/Documents/ULL_project/rangram_workdir/"
-SPPath="/home/andres/IdeaProjects/stream-parser/src/scripts/"
+HOME="/home/andres/"
+rangram_workdir="$HOME/Documents/ULL_project/rangram_workdir/"
+SPPath="$HOME/IdeaProjects/stream-parser/src/scripts/"
 
-workdir_path=$rangram_workdir$gram_name
+workdir_path=$rangram_workdir/$gram_name
 vocab_filename=$gram_name.vocab
 
-# TODO: Abort if ULL conda environment has not been acivated
-
 cd $workdir_path
-
-# Parse using SP and evaluate
 mkdir -p stream-parser
 cd stream-parser
 
 # Create vocabulary file
 $SPPath/dictionary_dir.sh ../corpus $vocab_filename
 
-# TODO: conda activate ull
-
+# Parse using SP and evaluate
 $SPPath/stream_evaluate.sh $vocab_filename ../corpus ../corpus ../GS $maxWinObserve $maxWinParse
-
-
-
