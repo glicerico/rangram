083a5d0a0dc2117f8b08f324353d7ed96bc77923
glicerico <suarezandres@gmail.com>
2019-12-04 18:06:29 +0800

    building converter

--- utils/conll2ull.py
+++ utils/conll2ull.py
@@ -3,11 +3,25 @@
 # runs on python3
 
 import sys
+import os.path
 
 
 def main(argv):
-    print(argv[0])
-    
+    sentence = ['ROOT']
+    link_ids = []
+    links = []
+
+    with open(argv[0], 'r') as fi:
+        lines = fi.readlines()
+        for line in lines:
+            if line == "\n":
+                sentence = ['ROOT']
+                for link in links_ids:
+                    links.append(li + " " + sentence[li] + " " + split_line[0] + " " + split_line[1])
+            else:
+                split_line = line.split('\t')
+                link_ids.append([split_line[6], split_line[0]])  # store links indexes
+                sentence.append(split_line[1]) # form sentence
 
 
 if __name__ == "__main__":
