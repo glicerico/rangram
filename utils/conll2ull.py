b872a2c632657c490942c7ded922f791ebc69293
glicerico <suarezandres@gmail.com>
2019-12-04 18:22:41 +0800

    basic converter working

--- utils/conll2ull.py
+++ utils/conll2ull.py
@@ -12,16 +12,22 @@ def main(argv):
     links = []
 
     with open(argv[0], 'r') as fi:
-        lines = fi.readlines()
-        for line in lines:
-            if line == "\n":
-                sentence = ['ROOT']
-                for link in links_ids:
-                    links.append(li + " " + sentence[li] + " " + split_line[0] + " " + split_line[1])
-            else:
-                split_line = line.split('\t')
-                link_ids.append([split_line[6], split_line[0]])  # store links indexes
-                sentence.append(split_line[1]) # form sentence
+        with open(argv[0] + ".ull", 'w') as fo:
+            lines = fi.readlines()
+            for line in lines:
+                if line == "\n":
+                    for link in link_ids:
+                        links.append(link[0] + " " + sentence[int(link[0])] + " " + link[1] + " " + sentence[int(link[1])])
+                    fo.write(" ".join(sentence[1:]) + "\n")
+                    links.sort()
+                    fo.write("\n".join(links) + "\n\n")
+                    # reset arrays
+                    sentence = ['ROOT']
+                    link_ids = []
+                else:
+                    split_line = line.split('\t')
+                    link_ids.append([split_line[6], split_line[0]])  # store links indexes
+                    sentence.append(split_line[1])  # form sentence
 
 
 if __name__ == "__main__":
