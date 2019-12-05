de2f24953c8b5a4e783d9b0731d312e51f5f83b5
glicerico <suarezandres@gmail.com>
2019-12-05 16:31:16 +0800

    working punctuation remover

--- utils/conll2ull.py
+++ utils/conll2ull.py
@@ -2,10 +2,48 @@
 # coding=utf-8
 # runs on python3
 
-
-
 import sys
-import os.path
+import re
+
+ROOT_WORD = "###LEFT-WALL###"
+IGNORED_WORD = "###PUNCTUATION###"
+IGNORED_FLAG = -1
+
+
+def tag_punctuation(sentence):
+    """
+    Tag every word that doesn't contain an alphanumeric character
+    """
+    tagged_sentence = []
+    tagged_len = 0
+    mapping = []
+    num_punctuations = 0  # count how many tokens are punctuation
+    for cnt, word in enumerate(sentence):
+        if bool(re.match('.?[A-Za-z0-9]', word)):
+            tagged_sentence.append(word)
+            mapping.append(cnt - num_punctuations)
+            tagged_len += 1
+        else:
+            tagged_sentence.append(IGNORED_WORD)
+            num_punctuations += 1
+            mapping.append(IGNORED_FLAG)
+
+    return tagged_sentence, tagged_len, mapping
+
+
+def create_links(sentence, mapping, link_ids):
+    """
+    Create links with correct numbering after punctuation removal
+    """
+    links = []
+    for link in link_ids:
+        mapped_ll = mapping[link[0]]  # mapped ids (adjusting for removed punctuation)
+        mapped_rl = mapping[link[1]]  # mapped ids (adjusting for removed punctuation)
+        if mapped_rl != IGNORED_FLAG and mapped_ll != IGNORED_FLAG:  # discard links with punctuation
+            links.append(str(mapped_ll) + " " + sentence[link[0]] + " " + str(mapped_rl) + " " + sentence[link[1]])
+
+    links.sort()
+    return links
 
 
 def main(argv):
@@ -13,11 +51,12 @@ def main(argv):
     Transforms dependency parses in CoNLL format to ULL format
     """
     conll_filename = argv[0]
-    # punctuation = argv[1]  # Flag to remove punctuation
-    # max_length = argv[2]  # max length of sentences to process after punctuation removal (if any)
-    char_set = set()
+    punct_flag = bool(int(argv[1]))  # Flag to remove punctuation
+    max_length = int(argv[2])  # max length of sentences to process after punctuation removal (if any)
+    print(punct_flag)
 
-    sentence = ['ROOT']
+    num_parses = 0  # num of parses in output file
+    sentence = [ROOT_WORD]
     link_ids = []
     links = []
 
@@ -25,25 +64,25 @@ def main(argv):
         with open(argv[0] + ".ull", 'w') as fo:
             lines = fi.readlines()
             for line in lines:
+                # Process parse when newline is found
                 if line == "\n":
-                    for link in link_ids:
-                        links.append(link[0] + " " + sentence[int(link[0])] + " " + link[1] + " " + sentence[int(link[1])])
-                    fo.write(" ".join(sentence[1:]) + "\n")
-                    links.sort()
-                    fo.write("\n".join(links) + "\n\n")
+                    tagged_sent, tagged_len, mapping = tag_punctuation(sentence)
+                    if tagged_len <= max_length:  # only print sentences within desired length
+                        links = create_links(tagged_sent, mapping, link_ids)
+                        clean_sent = [word for word in tagged_sent[1:] if word != IGNORED_WORD]
+                        fo.write(" ".join(clean_sent) + "\n")
+                        fo.write("\n".join(links) + "\n\n")
+                        num_parses += 1
                     # reset arrays
-                    sentence = ['ROOT']
+                    sentence = [ROOT_WORD]
                     link_ids = []
-                    links = []
+                # Links are still being processed
                 else:
                     split_line = line.split('\t')
-                    link_ids.append([split_line[6], split_line[0]])  # store links indexes
+                    link_ids.append([int(split_line[6]), int(split_line[0])])  # store links indexes
                     sentence.append(split_line[1])  # build sentence array
-                    for curr_char in split_line[1]:
-                        char_set.add(curr_char)
 
-    print(char_set)
-    print(len(char_set))
+    print(f"Printed {num_parses} parses with len <= {max_length}")
 
 
 if __name__ == "__main__":
